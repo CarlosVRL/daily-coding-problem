e4e7c80f423783240d9fb730589a38a7d9518cdb
CarlosVRL <carlos.varela.eng@gmail.com>
2018-10-07 01:43:50 -0500

    Solution for Problem 1 in "one pass"

--- src/problems/problem_1.py
+++ src/problems/problem_1.py
@@ -5,6 +5,8 @@ Given a list of numbers and a number k, return whether any two numbers from the
 list add up to k.
 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
 """
 
 
@@ -12,16 +14,16 @@ def check_numbers_for_sum(numbers, k):
 
     sum_found = False
 
-    for i, outer_number in enumerate(numbers):
+    previous_numbers = []
 
-        a = outer_number
+    for number in numbers:
 
-        for j, inner_number in enumerate(numbers):
-            if i == j:
-                continue
-            b = inner_number
-            if (a + b) == k:
+        for previous_number in previous_numbers:
+
+            if number + previous_number == k:
                 sum_found = True
                 return sum_found
+
+        previous_numbers.append(number)
 
     return sum_found
